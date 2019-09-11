6695496ab524a231f888bda39f24f8153b8f3f3d
piyushagwl007 <piyushagwl007@gmail.com>
2019-09-12 01:15:19 +0530

    feat:add swagger plugin with sample json

--- public-src/App.jsx
+++ public-src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
+import SwaggerUI from 'swagger-ui-react'
+import 'swagger-ui-react/swagger-ui.css'
 
 function App () {
   return (
-    <div>
-      <h1> Hello, World! </h1>
-    </div>
+    <SwaggerUI url='https://petstore.swagger.io/v2/swagger.json' />
   )
 }
 
