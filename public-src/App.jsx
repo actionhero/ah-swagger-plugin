e4ccb4cb6e07cdeaffde2863c0807a10058aedbf
piyushagwl007 <piyushagwl007@gmail.com>
2019-09-18 14:55:37 +0530

    fix: change the fallback and make static css url

--- public-src/App.jsx
+++ public-src/App.jsx
@@ -5,7 +5,7 @@ import 'swagger-ui-react/swagger-ui.css'
 function App () {
   const SwaggerComponent = lazy(() => import('swagger-ui-react'))
   return (
-    <Suspense fallback={<div />}>
+    <Suspense fallback={<h2>Loading Swagger Plugin...</h2>}>
       <SwaggerComponent url='https://petstore.swagger.io/v2/swagger.json' />
     </Suspense>
   )
--- webpack.config.js
+++ webpack.config.js
@@ -59,8 +59,8 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // all options are optional
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      filename: devMode ? '[name].css' : '[name].css',
+      chunkFilename: devMode ? '[id].css' : '[id].css',
       ignoreOrder: false // Enable to remove warnings about conflicting order
     })
   ]
